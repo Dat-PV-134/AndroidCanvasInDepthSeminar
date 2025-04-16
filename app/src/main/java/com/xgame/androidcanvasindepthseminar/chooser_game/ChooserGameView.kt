29f75b29d6d38f0e4f4ae3ebff6a882ba83119e6
Phạm Văn Đạt <94840545+Dat-PV-134@users.noreply.github.com>
2025-04-16 15:14:25 +0700

    last step

--- app/src/main/java/com/xgame/androidcanvasindepthseminar/chooser_game/ChooserGameView.kt
+++ app/src/main/java/com/xgame/androidcanvasindepthseminar/chooser_game/ChooserGameView.kt
@@ -5,25 +5,35 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Point
 import android.graphics.PointF
+import android.os.CountDownTimer
 import android.util.AttributeSet
 import android.util.Log
-import android.util.SparseArray
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.core.graphics.toColorInt
-import androidx.core.util.isEmpty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.collections.set
 
 class ChooserGameView : View {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var centerX = 0f
-    private var centerY = 0f
     private val circles = mutableListOf<Circle>()
     private val circleCount = 5
 
     private val userTouchPoints by lazy { HashMap<Int, PointF?>() }
+
+    private var timer: CountDownTimer? = null
+    private var elapsedTime = 0L
+    private var isTimerRunning = false
+
+    private var winner: PointF? = null
+    private var isWinningTime = false
+
 
     private data class Circle(
         var radius: Float = 0f,
@@ -43,12 +53,8 @@ class ChooserGameView : View {
     )
 
     init {
-        post {
-            centerX = width / 2f
-            centerY = height / 2f
-            setupCircles()
-            startAnimation()
-        }
+        setupCircles()
+        startAnimation()
     }
 
     private fun setupCircles() {
@@ -82,67 +88,82 @@ class ChooserGameView : View {
         }
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        centerX = w / 2f
-        centerY = h / 2f
-        setupCircles()
-    }
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        userTouchPoints.forEach { touchPoint ->
-            // Vẽ từ ngoài vào trong để vòng trong đè lên vòng ngoài
-            for (i in circles.size - 1 downTo 0) {
-                val circle = circles[i]
-                paint.apply {
-                    style = Paint.Style.FILL
-                    color = "#4CAF50".toColorInt()
-                    alpha = circle.alpha
-                }
-
+        if (!isWinningTime) {
+            userTouchPoints.forEach { touchPoint ->
                 touchPoint.value?.let {
-                    // Vẽ vòng tròn với scale hiện tại
-                    canvas.drawCircle(
-                        it.x,
-                        it.y,
-                        circle.radius * circle.currentScale,
-                        paint
-                    )
+                    drawCircleAnim(canvas, it)
                 }
+            }
+        } else {
+            winner?.let {
+                drawCircleAnim(canvas, it)
+            }
+        }
+    }
+
+    private fun drawCircleAnim(canvas: Canvas, point: PointF) {
+        for (i in circles.size - 1 downTo 0) {
+            val circle = circles[i]
+            paint.apply {
+                style = Paint.Style.FILL
+                color = "#4CAF50".toColorInt()
+                alpha = circle.alpha
+            }
+
+            point.let {
+                // Vẽ vòng tròn với scale hiện tại
+                canvas.drawCircle(
+                    x,
+                    y,
+                    circle.radius * circle.currentScale,
+                    paint
+                )
             }
         }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        event?.let { event ->
-            when (event.actionMasked) {
-                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> {
-                    userTouchPoints.put(
-                        event.getPointerId(event.actionIndex),
-                        PointF(event.getX(event.actionIndex), event.getY(event.actionIndex))
-                    )
-                    userTouchPoints.forEach {
-                        Log.e("DatPV", it.toString())
-                    }
-                }
+        if (!isWinningTime) {
+            event?.let { event ->
+                when (event.actionMasked) {
+                    MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> {
+                        userTouchPoints.put(
+                            event.getPointerId(event.actionIndex),
+                            PointF(event.getX(event.actionIndex), event.getY(event.actionIndex))
+                        )
+                        userTouchPoints.forEach {
+                            Log.e("DatPV", it.toString())
+                        }
 
-                MotionEvent.ACTION_MOVE -> {
-                    for (i in 0 until event.pointerCount) {
-                        val pointerId = event.getPointerId(i)
-                        userTouchPoints[pointerId]?.let { point ->
-                            point.x = event.getX(i)
-                            point.y = event.getY(i)
-                        } ?: run {
-                            userTouchPoints[pointerId] = PointF(event.getX(i), event.getY(i))
+                        if (userTouchPoints.size >= 2) {
+                            stopTimer()
+                            startTimer()
                         }
                     }
-                }
 
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_CANCEL -> {
-                    userTouchPoints.remove(event.getPointerId(event.actionIndex))
+                    MotionEvent.ACTION_MOVE -> {
+                        for (i in 0 until event.pointerCount) {
+                            val pointerId = event.getPointerId(i)
+                            userTouchPoints[pointerId]?.let { point ->
+                                point.x = event.getX(i)
+                                point.y = event.getY(i)
+                            } ?: run {
+                                userTouchPoints[pointerId] = PointF(event.getX(i), event.getY(i))
+                            }
+                        }
+                    }
+
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_CANCEL -> {
+                        userTouchPoints.remove(event.getPointerId(event.actionIndex))
+
+                        if (userTouchPoints.size < 2) {
+                            stopTimer()
+                        }
+                    }
                 }
             }
         }
@@ -150,7 +171,37 @@ class ChooserGameView : View {
         return true
     }
 
+    private fun startTimer() {
+        isWinningTime = false
+        isTimerRunning = true
+        elapsedTime = 0L
+
+        timer = object : CountDownTimer(4000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                winner = userTouchPoints.values.random()
+                isWinningTime = true
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(3000)
+                    userTouchPoints.clear()
+                    isWinningTime = false
+                    winner = null
+                }
+            }
+        }.start()
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+        timer = null
+        isTimerRunning = false
+    }
+
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
+        stopTimer()
     }
 }
